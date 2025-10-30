78223bd6d0a16d32ab4ad207a32c1d219fc981ca
Mohammedharris10 <s.mohammedharris10@gmail.com>
2025-10-30 16:44:05 +0530

    Create login API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,11 @@
 const catchAsyncError = require("../middlewares/catchAsyncError");
-const User = require("../models/userModel")
+const User = require("../models/userModel");
+const ErrorHandler = require("../middlewares/error");
+const sendToken = require("../utils/jwt")
 
-exports.registerUser = catchAsyncError(async(req, res, next)=>{
-    const {name, email, password, avatar} = req.body;
+//Creating User registation API -
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+    const { name, email, password, avatar } = req.body;
     await User.deleteMany({});
     const user = await User.create({
         name,
@@ -11,11 +14,28 @@ exports.registerUser = catchAsyncError(async(req, res, next)=>{
         avatar
     })
 
-    const token = user.getJwtToken();
+    sendToken(user, 201, res)
+})
 
-    res.status(201).json({
-        success: true,
-        user,
-        token
-    })
+
+//Creating login API - 
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+
+    const { email, password } = req.body; // get email & password
+
+    if (!email || !password) {
+        return next(new ErrorHandler("Please enter email & password", 400))
+    }
+
+    const user = await User.findOne({ email }).select('+password'); // find user and get password
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 400))
+    }
+
+    if (!(await user.isValidPassword(password))) { // check password match or not
+        return next(new ErrorHandler("Invalid email or password", 400))
+    }
+
+    sendToken(user, 201, res) // send jwt token if login success
 })
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,12 +4,12 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: [true, "Please enter the name"]
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
         required: [true, "Please enter email"],
         unique: true, //unique value that's why mentioned this
         validator: [validator.isEmail, "Please enter valid email"]
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Plaese enter password"],
         maxlength: [6, 'Password cannot exceed 6 characters']
+        , select: false // // don't show password when fetching user data
     },
     avatar: {
         type: String,
@@ -35,18 +36,24 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-userSchema.pre("save", async function (next){
+userSchema.pre("save", async function (next) {
     // before saving user, hash the password before (pre) a document is saved to the database.
     this.password = await bcrypt.hash(this.password, 10)
     // becasue of we never save plain password in database
 })
 
-userSchema.methods.getJwtToken = function(){
-    return jwt.sign({id: this.id}, process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRES_TIME
+userSchema.methods.getJwtToken = function () {
+     // create jwt token using user id
+    return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_TIME //token expire time
     })
 }
 
-let model = mongoose.model('User',userSchema);
+userSchema.methods.isValidPassword = async function(enterPassword){
+    // compare entered password with hashed password
+    await bcrypt.compare(enterPassword, this.password)
+}
+
+let model = mongoose.model('User', userSchema);
 
 module.exports = model;
