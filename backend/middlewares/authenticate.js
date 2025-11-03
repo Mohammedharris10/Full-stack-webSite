f146102787be51e9bf233fd212e4f9c9804c8ee8
Mohammedharris10 <s.mohammedharris10@gmail.com>
2025-11-03 11:22:57 +0530

    Add Authorized user role middleware

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -6,13 +6,22 @@ const jwt = require('jsonwebtoken');
 
 
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) =>{
-    const {token} = req.cookies;
+    const {token} = req.cookies; // get token from cookies
 
     if(!token){
-        return next(new ErrorHandler('Login first to handle this',401));
+        return next(new ErrorHandler('Login first to handle this',401));  // if no token, stop request
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = await User.findById(decoded.id)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)  // verify token with secret key
+    req.user = await User.findById(decoded.id) // find user
     next();
 })
+
+exports.authorizeRoles = (...roles) =>{
+    return (req, res, next) =>{
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`The role ${req.user.role} is not allowed`,401))
+        }
+        next();
+    }
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const router = express.Router();
-const {isAuthenticatedUser }= require('../middlewares/authenticate');
+const {isAuthenticatedUser, authorizeRoles }= require('../middlewares/authenticate');
  
 router.route('/products').get(isAuthenticatedUser,getProducts); //those functions (getProduct, newProduct....) come from ProductController.js
-router.route('/product/new').post(newProduct);
+router.route('/product/new').post(isAuthenticatedUser,authorizeRoles('admin'),newProduct); //Check Login > Admin or user > access the newProduct
 router.route('/product/:id')
                             .delete(deleteProduct)
                             .get(getSingleProduct)
